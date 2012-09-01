349b905ce4737bdb73c7ad4f7a7b8399459d5cd1
Steven Matthews <steven.matthews@live.co.uk>
2012-09-01 23:02:06 +0200

    Added a very basic example of Factory Pattern
    This very basic example of a factory pattern in action will demostrate
    how programming to the interface instead of the concrete implementation
    means we are not bound to either the Dog or Cat type. As long as both
    classes implement the same interface, mypet can be either a Dog or a
    Cat.

---
+++ PatternsScratchArea/PatternsConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using PatternsScratchArea;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnimalFactory factory = new AnimalFactory();
+            IAnimal mypet = factory.GetAnimalBySound("Woof");
+            //Do some dog work
+            mypet = factory.GetAnimalBySound("Meow");
+            //Now my pet is a cat
+
+        }
+
+    }
+}
---
+++ PatternsScratchArea/PatternsScratchArea/FactoryPattern.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternsScratchArea
+{
+    public interface IAnimal
+    {
+
+        string Eat();
+
+        string Sleep();
+
+    }
+    public class Dog : IAnimal
+    {
+
+        public string Eat()
+        {
+            //do some work here
+            return "Yum";
+        }
+
+        public string Sleep()
+        {
+            // do some work here
+            return "snore";
+        }
+    }
+
+    public class Cat : IAnimal
+    {
+
+        public string Eat()
+        {
+            // do some work
+            return "Yum";
+        }
+
+        public string Sleep()
+        {
+            //do some work
+            return "snore";
+        }
+    }
+
+    public class AnimalFactory
+    {
+        public IAnimal GetAnimalBySound(string animalNoise)
+        {
+            IAnimal animal = null;
+            switch (animalNoise)
+            {
+                case "Woof":
+                    animal = new Dog();
+                    break;
+
+                case "Meow":
+                    animal = new Cat();
+                    break;
+            }
+            return animal;
+        }
+    }
+
+
+
+}
